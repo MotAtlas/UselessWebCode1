8e13379d943a1e1a56f1311f834bb98a57edf0da
AndreaG <motatlas@gmail.com>
2019-01-16 09:24:28 +0100

    dunno

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>test1</title>
+    </head>
+    <body>
+        <h1>test1</h1>
+        <p>Your new vhost is alive!</p>
+        <?php phpinfo(); ?>
+    </body>
+</html>
